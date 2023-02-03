ad6530827eb0be187b98ae45fa443570c761d229
kaushik <kaushik116k@gmail.com>
2023-02-03 12:41:43 +0530

    changes made

--- lib/ship.dart
+++ lib/ship.dart
@@ -6,7 +6,7 @@ class ship extends CustomClipper<Path>{
 
     var radius = 30.0;
     Path path = Path();
-    /*path.moveTo(0, size.height/2);
+    path.moveTo(0, size.height/2);
     path.arcToPoint(Offset(size.width/8, size.height/4), clockwise: true, radius: Radius.circular(radius));
     path.lineTo(size.width/4, size.height/4);
     path.lineTo(size.width/2, 0);
@@ -15,13 +15,13 @@ class ship extends CustomClipper<Path>{
     path.arcToPoint(Offset(size.width, size.height/2), clockwise: true, radius: Radius.circular(radius));
     path.lineTo(size.width, size.height);
     path.lineTo(0, size.height);
-    path.lineTo(0, size.height/2);*/
+    path.lineTo(0, size.height/2);
 
-    path.moveTo(0, size.height);
+    /*path.moveTo(0, size.height);
     path.lineTo(size.width/2, 0);
     path.lineTo(size.width, size.height);
     path.lineTo(0, size.height);
-    path.close();
+    path.close();*/
     return path;
   }
 
